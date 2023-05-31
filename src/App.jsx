55626f01c52491bb3164fe896e5b2e47c639adab
Ose-Augustine <osebeckley@gmail.com>
2023-05-31 16:28:47 +0100

    Completed header

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 export default function App() {
   return (
     <div>
-      <p>this is the start</p>
+     <header className="text-white bg-black p-6 flex justify-between justify-items-center">
+      <div>RealEstate &reg;</div>
+      <div>
+        <a className="mr-4"href="#">HOME</a>
+        <a className="mr-4"href="#">FORSALE</a>
+        <a className="mr-4"href="#">FORRENT</a>
+        <a className="mr-4"href="#">ABOUT</a>
+      </div>
+      <button className="rounded-full px-8 py-4 border-2 border-white">Free Consult</button>
+     </header>
     </div>
   )
 }
