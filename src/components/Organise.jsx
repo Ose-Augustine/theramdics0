b2ce65f0ba1fad5575c19a33392a207f06536b2b
Ose-Augustine <osebeckley@gmail.com>
2023-06-01 07:16:38 +0100

    Style fixing

--- src/components/Organise.jsx
+++ src/components/Organise.jsx
@@ -1,9 +1,9 @@
 export default function Organise() {
     return(
         <div className="relative border-white border-2 h-full">
-            <div className="uppercase absolute bottom-0">
-                <p> organised</p>
-                <p className="text-[#A5A5A5]">We offer the most beautiful homes and <br /> apartments available for rent and sales</p>
+            <div className="uppercase absolute bottom-4 mx-6">
+                <p className="mb-4 text-[40px]"> organised</p>
+                <p className="text-[#A5A5A5] text-[16px]">We offer the most beautiful homes and <br /> apartments available for rent and sales</p>
             </div>
         </div>
     )
