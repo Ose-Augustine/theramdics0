6a5a05007f4fabd6af1cdd0662e91953fcee6ff6
Ose-Augustine <osebeckley@gmail.com>
2023-06-01 20:55:29 +0100

    Completed entire layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Hero from "./components/Hero";
+import NestedIcon from "./components/NestedIcon";
 import Organise from "./components/Organise";
 import Time from "./components/Time";
 
@@ -63,15 +64,66 @@ export default function App() {
             <div className="w-1/3 ">
               <Organise />
             </div>
-            <div className="border-white border-2 grid grid-cols-2">
+            <div className="border-white border-2 grid grid-cols-2 border-l-transparent">
               <Organise />
               <img src="/Rectangle4.jpg" alt="Dimly lit room with dark sofa and wall pictures" />
               <img src="./Rectangle3.jpg" alt="Golden lamps" />
               <Organise />
             </div>
         </div>
+        <section className=" uppercase pl-20 text-[96px] leading-[115px]">
+          <div>
+          Impeccable quality, <br /> exquisite design, and <br />premium materials are <br /> the hallmarks of our <br /> collections. 
+          </div>
+          <div className="flex justify-evenly">
+            <div>
+              <span>01/20</span>
+              <span className="text-[40px]">pcs</span>
+            </div>
+            <div>
+              -
+            </div>
+            <div>
+              2023
+            </div>
+          </div>
+        </section>
+        <section className="flex w-screen min-h-max border-y-2 border-white">
+          <div className="bg-white text-black mr-6 w-1/3 p-8">
+            <p className="text-[163px] uppercase text-clip leading-[137px] mb-10">RE <br /> alE <br />STA <br /> TE</p>
+            <div className="[&>*]:mb-5 [&>*>*]:mr-3">
+              <div>
+                 <button className="border-2 border-black p-2  rounded-full">CHAIRS</button><button className="border-2 border-black p-2  rounded-full">TABLES</button>
+              </div>
+              <div>
+                 <button className="border-2 border-black p-2  rounded-full">OAK</button><button className="border-2 border-black p-2   rounded-full">WALNUT</button>
+              </div>
+              <div>
+                <button className="border-2 border-black p-2   rounded-full">LEATHER</button><button className="border-2 border-black p-2  rounded-full">CLOTH</button>
+              </div>
+            </div>
+          </div>
+          <div className="min-h-full w-2/3 text-[50px] flex flex-col justify-end p-10" style={{backgroundImage: 'url(./Rectangle5.jpg)'}}>
+              <div>
+                <span className="mr-16">PRODUCT NAME</span><span>$5,430</span>
+              </div>
+              <p className="text-[20px]">CATEGORY</p>
+          </div>
+        </section>
+        <section>
+          <p className="text-[280px]">RealEstate</p>
+        </section>
      </main>
-     
+     <footer className="bg-black bg-opacity-95 flex justify-evenly">
+        <NestedIcon />
+        <div>
+          <p className="border-b-[#545454]  text-[#545454] text-[128px] leading-[124px]">love our stuff? <br /> send us an email.</p>
+          <p className="">
+            <i className="border-white border-2 rounded-full p-4"><img src="./facebook.jpg" alt="facebook icon" /></i><i className="border-white border-2 rounded-full p-4"><img src="./instagram.jpg" alt="instagram icon" /></i><i className="border-white border-2 rounded-full p-4"><img src="linkedin.jpg" alt="linkedin icon" /></i>
+            
+          </p>
+        </div>
+     </footer>
     </div>
   )
 }
---
+++ src/components/NestedIcon.jsx
@@ -0,0 +1,9 @@
+export default function NestedIcon(){
+    return(
+        <div>
+            <div>
+                <i></i>
+            </div>
+        </div>
+    )
+}
